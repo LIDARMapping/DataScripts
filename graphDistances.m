2a39169b59a6261940eceaf1ee4ad99b86b02f67
EdWard680 <ezeward4@gmail.com>
2015-07-09 16:13:53 -0400

    Adds distance trendline graph

---
+++ graphDistances.m
@@ -0,0 +1,25 @@
+close all
+clear all
+
+% Load stuff
+file = input("Data file name: ");
+data = load(file);
+realDist = data(:,1);
+response = data(:,2);
+figure
+
+scatter(realDist, response);
+hold on;
+
+mypoly = polyfit(realDist, response, 2);
+
+xTrend = 1.0:0.01:10;
+yTrend = polyval(mypoly, xTrend);
+plot(xTrend, yTrend);
+
+slope = (yTrend(end) - yTrend(1)) /(xTrend(end) - xTrend(1))
+intercept = yTrend(1) - slope * xTrend(1)
+
+xlabel('actual distance (meters)');
+ylabel('response ratio');
+title('LiDAR distance ratios with white paper');
--- graphLaser.m
+++ graphLaser.m
@@ -2,7 +2,8 @@ close all
 clear all
 
 % Load stuff
-data = load("lidarConsistentData.txt");
+file = input("Data file name: ");
+data = load(file);
 t = data(:,4);
 sync = data(:,3);
 zero = data(:,1);
@@ -11,7 +12,7 @@ figure
 
 % Ret & Zero
 plot(t, ret, t, zero, t, sync);
-axis([0 3000 0 5]);
+axis([0 2000 0 5]);
 xlabel('time (ms)');
 ylabel('voltage and high/low');
 title('LiDAR sync, return, and zero pins vs. time');
